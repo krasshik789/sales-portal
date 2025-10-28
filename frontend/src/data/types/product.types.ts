6a9db9e7c32b35dbb9d03fa3506842befbfbe7a7
krasshik789 <gelis91007@gmail.com>
2025-10-29 00:36:04 +0300

    finished task 2 hw-22

--- frontend/src/data/types/product.types.ts
+++ frontend/src/data/types/product.types.ts
@@ -6,3 +6,8 @@ export interface IProduct {
   amount: number;
   notes?: string;
 }
+export interface ICreatedOn {
+  createdOn: string;
+}
+
+export interface IProductInTable extends Pick<IProduct, 'name' | 'manufacturer' | 'price'>, ICreatedOn {}
--- frontend/src/ui/pages/products/productsList.page.ts
+++ frontend/src/ui/pages/products/productsList.page.ts
@@ -1,14 +1,32 @@
 import { SalesPortalPage } from "../salesPortal.page";
+import { IProductInTable } from "../../../data/types/product.types";
+import {MANUFACTURERS} from "../../../data/salesPortal/products/manufacturers" 
 
 export class ProductsListPage extends SalesPortalPage {
   readonly productsPageTitle = this.page.locator("h2.fw-bold");
   readonly addNewProductButton = this.page.locator('[name="add-button"]');
+
   readonly tableRowByName = (productName: string) =>
-    this.page.locator("table tbody tr", { has: this.page.locator("td", { hasText: productName }) });
+    this.page.locator(
+      "table tbody tr",
+      { has: this.page.locator("td", { hasText: productName }) }
+    );
 
   readonly uniqueElement = this.addNewProductButton;
 
   async clickAddNewProduct() {
     await this.addNewProductButton.click();
+  }
+
+  async getLastProduct(productName: string): Promise<IProductInTable> {
+    const [name, price, manufacturer, createdOn] =
+      await this.tableRowByName(productName).locator("td").allInnerTexts();
+
+    return {
+      name: name!,
+      price: +price!.replace("$", ""),
+      manufacturer: manufacturer! as MANUFACTURERS,
+      createdOn: createdOn!,
+    };
   }
 }
